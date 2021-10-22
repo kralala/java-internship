0eb9c2d5a29d5f41d68fa2203f6b1219ce4e2070
kralala <82995633+kralala@users.noreply.github.com>
2021-10-22 13:14:19 +0300

    Update School.java

--- src/main/java/hiber/model/School.java
+++ src/main/java/hiber/model/School.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class School {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private UUID id;
 
     @Column(name = "name")
