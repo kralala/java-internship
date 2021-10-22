22ce2cb0be3fbca51ee0feb46034eb913daca6fb
kralala <82995633+kralala@users.noreply.github.com>
2021-10-22 12:46:00 +0300

    Update User.java

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -20,7 +20,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name = "id")
     private UUID id;
 
     @Column(name = "first_name")
