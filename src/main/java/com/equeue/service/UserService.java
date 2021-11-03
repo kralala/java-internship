f27797b5e511aa6c04bf477fccc297b73e26e8ee
kralala <82995633+kralala@users.noreply.github.com>
2021-11-03 13:24:37 +0300

    Update UserService.java

--- src/main/java/com/equeue/service/UserService.java
+++ src/main/java/com/equeue/service/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
